2b254582b2c60f93d6b8a8a5fc0cec156902351b
Leon Wright <techman83@gmail.com>
2019-06-25 19:42:00 +0800

    Some intial fleshing out of the Message + Queue handling

--- indexer/netkan-indexer.py
+++ indexer/netkan-indexer.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 
 import click
-import boto3
 
 @click.command()
 @click.option('--queue', default='OutboundDev.fifo', help='SQS Queue to poll for metadata')
 @click.option('--metadata', help='Queue Path/Url for Submitting Metadata', required=True,  envvar='METADATA_REPO')
 def run(queue, metadata):
-    sqs = boto3.resource('sqs')
-    queue = sqs.get_queue_by_name(QueueName=queue)
-    print(queue.url)
+    #sqs = boto3.resource('sqs')
+    #queue = sqs.get_queue_by_name(QueueName=queue)
+    #print(queue.url)
+    print(metadata)
 
 
 if __name__ == '__main__':
+    local_meta = Path('/tmp/CKAN-meta')
     run()
 
