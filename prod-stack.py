dc42718370d986ea768e1488c3f83b149942e4a8
Leon Wright <techman83@gmail.com>
2019-08-29 18:45:02 +0800

    Add initial EC2 config

--- prod-stack.py
+++ prod-stack.py
@@ -1,15 +1,20 @@
 # Converted from SQS_With_CloudWatch_Alarms.template located at:
 # http://aws.amazon.com/cloudformation/aws-cloudformation-templates/
 
-from troposphere import GetAtt, Output, Ref, Template
+from troposphere import GetAtt, Output, Ref, Template, Sub, Base64
 from troposphere.iam import Policy, Role, InstanceProfile
 from troposphere.sqs import Queue
 from troposphere.dynamodb import Table, KeySchema, AttributeDefinition, \
     ProvisionedThroughput
+from troposphere.ecs import Cluster
+from troposphere.ec2 import Instance, CreditSpecification
+from troposphere.cloudformation import Init, InitFile, InitFiles, \
+    InitConfig, Metadata
 import os
 import sys
 
 zone_id = os.environ.get('CKAN_ZONEID', False)
+status_fqdn = 'status.test.ksp-ckan.space'
 
 if not zone_id:
     print('Zone ID Required from EnvVar `CKAN_ZONEID`')
@@ -22,11 +27,11 @@ t.set_description("Generate NetKAN Infrastructure CF Template")
 # Inbound + Outbound SQS Queues
 # Inbound: Scheduler Write, Inflation Read
 # Outbound: Inflator Write, Indexer Read
-inbound = t.add_resource(Queue("Inbound",
+inbound = t.add_resource(Queue("NetKANInbound",
                                QueueName="Inbound.fifo",
                                ReceiveMessageWaitTimeSeconds=20,
                                FifoQueue=True))
-outbound = t.add_resource(Queue("Outbound",
+outbound = t.add_resource(Queue("NetKANOutbound",
                                 QueueName="Outbound.fifo",
                                 ReceiveMessageWaitTimeSeconds=20,
                                 FifoQueue=True))
@@ -100,6 +105,9 @@ netkan_role = t.add_resource(Role(
             }
         ]
     },
+    ManagedPolicyArns=[
+        "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role",
+    ],
     Policies=[
         Policy(
             PolicyName="SQSProdPolicy",
@@ -212,5 +220,78 @@ netkan_profile = t.add_resource(InstanceProfile(
     "NetKANProdProfile",
     Roles=[Ref(netkan_role)]
 ))
+
+# Indexer Compute
+# We could utilise an autoscaling group, but that is way
+# more complicated for our use case. If at some point we'd
+# to scale the service beyond a single instance (due to some
+# infrastructure sponsorship) it wouldn't take more than
+# adding an AutoScalingGroup + LoadBalancer to scale this.
+netkan_ecs = t.add_resource(Cluster("NetkanCluster"))
+
+netkan_userdata = Sub("""
+#!/bin/bash -xe
+yum install -y aws-cfn-bootstrap
+# Install the files and packages from the metadata
+/opt/aws/bin/cfn-init -v --stack ${AWS::StackName} \
+--resource NetKANCompute --region ${AWS::Region}
+# Start up the cfn-hup daemon to listen for changes
+# to the metadata
+/opt/aws/bin/cfn-hup || error_exit 'Failed to start cfn-hup
+# Signal the status from cfn-init
+/opt/aws/bin/cfn-signal -e $? --stack ${AWS::StackName} \
+--resource EcsInstance --region ${AWS::Region}
+""")
+
+cfn_hup = InitFile(
+    content=Sub(
+        "[main]\nstack=${AWS::StackId}\nregion=${AWS::Region}\n"
+    ),
+    mode='000400',
+    owner='root',
+    group='root'
+)
+reloader = InitFile(
+    content=Sub("""
+[cfn-auto-reloader-hook]
+triggers=post.add, post.update
+path=Resources.NetKANCompute.Metadata.AWS::CloudFormation::Init
+action=/opt/aws/bin/cfn-init -s ${AWS::StackId} -r NetKANCompute --region ${AWS::Region}
+runas=root
+""")
+)
+docker = InitFile(
+    content="""
+{
+    "log-driver": "json-file",
+    "log-opts": {
+        "max-size": "20m",
+        "max-file": "3",
+    }
+}
+""")
+netkan_instance = Instance(
+    'NetKANCompute',
+    # ECS Optimised us-west-2
+    ImageId='ami-0e434a58221275ed4',
+    InstanceType='t3.micro',
+    IamInstanceProfile=Ref(netkan_profile),
+    KeyName='techman83_work',
+    SecurityGroups=[],
+    UserData=Base64(netkan_userdata),
+    # t3 instances are unlimited by default
+    CreditSpecification=CreditSpecification(CPUCredits='standard'),
+    Metadata=Metadata(Init({
+        'config': InitConfig(
+            files=InitFiles({
+                '/etc/cfn/cfn-hup.conf': cfn_hup,
+                '/etc/cfn/hooks.d/cfn-auto-reloader.conf': reloader,
+                '/etc/docker/daemon.json': docker,
+            })
+        )
+    })),
+)
+
+t.add_resource(netkan_instance)
 
 print(t.to_yaml())
