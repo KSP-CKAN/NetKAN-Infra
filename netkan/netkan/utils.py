d4859537b155b9999452fdb79c030e165c151bf8
Leon Wright <techman83@gmail.com>
2019-09-11 16:50:31 +0800

    'clone_from' seemed to mess up the refs mapping in origin, breaking staging entirely

--- netkan/netkan/utils.py
+++ netkan/netkan/utils.py
@@ -9,8 +9,13 @@ def init_repo(metadata, path):
     clone_path = Path(path)
     if not clone_path.exists():
         logging.info('Cloning {}'.format(metadata))
-        Repo.clone_from(metadata, clone_path, depth=1)
-    return Repo(clone_path)
+        clone_path.mkdir(parents=True)
+        repo = Repo.init(clone_path)
+        repo.create_remote('origin', metadata)
+        repo.remotes.origin.pull('master:master')
+    else:
+        repo = Repo(clone_path)
+    return repo
 
 
 def init_ssh(key, path):
