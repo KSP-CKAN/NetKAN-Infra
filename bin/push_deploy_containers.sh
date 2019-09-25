fd907bc67713783f3f7afdce2896a8868e162fac
Leon Wright <techman83@gmail.com>
2019-09-25 16:59:04 +0800

    There is a lot of risk involved rebuilding the legacy containers on every build, lets not do that

--- bin/push_deploy_containers.sh
+++ bin/push_deploy_containers.sh
@@ -4,12 +4,6 @@
 docker tag "kspckan/netkan" "kspckan/netkan:latest"
 docker push "kspckan/netkan:latest"
 
-# Legacy Webhooks
-docker pull "kspckan/webhooks" || true
-docker build webhooks/. -t kspckan/webhooks
-docker tag "kspckan/webhooks" "kspckan/webhooks:latest"
-docker push "kspckan/webhooks:latest"
-
 # Webhooks Proxy
 docker pull "kspckan/webhooks-proxy" || true
 docker build nginx/. -t kspckan/webhooks-proxy
