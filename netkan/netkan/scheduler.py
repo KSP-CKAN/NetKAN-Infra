701d2862371cd7502a541bd79656da3741dba72a
HebaruSan <HebaruSan@users.noreply.github.com>
2019-11-14 19:36:44 -0600

    Make Scheduler sorting case insensitive (#94)

--- netkan/netkan/scheduler.py
+++ netkan/netkan/scheduler.py
@@ -26,7 +26,8 @@ class NetkanScheduler:
     def netkans(self):
         # This can easily be recursive with '**/*.netkan', however
         # implementing like for like initially.
-        return (Netkan(f) for f in sorted(self.path.glob('*.netkan')))
+        return (Netkan(f) for f in sorted(self.path.glob('*.netkan'),
+                                          key=lambda p: p.stem.casefold()))
 
     def sqs_batch_attrs(self, batch):
         return {
