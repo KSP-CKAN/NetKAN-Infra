88ad3d63751622adcdee1a43cf3686e8a75fb4f6
Paul Hebble <pjhebble@gmail.com>
2020-01-28 17:05:40 -0600

    Fix SpaceDock API format and docs

--- netkan/netkan/webhooks/spacedock_add.py
+++ netkan/netkan/webhooks/spacedock_add.py
@@ -10,26 +10,22 @@ spacedock_add = Blueprint('spacedock_add', __name__)  # pylint: disable=invalid-
 
 # For mod creation hook on SpaceDock, creates pull requests
 # Handles: https://netkan.ksp-ckan.space/sd/add
-# POST json input:
-#   {
-#     "name":              "Mod Name Entered by the User on SpaceDock",
-#     "id":                "12345",
-#     "license":           "GPL-3.0",
-#     "username":          "ModAuthor1",
-#     "email":             "modauthor1@gmail.com",
-#     "short_description": "A mod that you should definitely install",
-#     "description":       "A mod that you should definitely install, and so on and so on",
-#     "external_link":     "https://forum.kerbalspaceprogram.com/index.php?/topic/999999-ThreadTitle",
-#     "user_url":          "https://spacedock.info/profile/ModAuthor1",
-#     "mod_url":           "https://spacedock.info/mod/12345",
-#     "site_name":         "SpaceDock"
-#   }
+# POST form parameters:
+#     name:              Mod Name Entered by the User on spacedock
+#     id:                12345
+#     license:           GPL-3.0
+#     username:          modauthor1
+#     email:             modauthor1@gmail.com
+#     short_description: A mod that you should definitely install
+#     description:       A mod that you should definitely install, and so on and so on
+#     external_link:     https://forum.kerbalspaceprogram.com/index.php?/topic/999999-ThreadTitle
+#     user_url:          https://spacedock.info/profile/ModAuthor1
+#     mod_url:           https://spacedock.info/mod/12345
+#     site_name:         SpaceDock
 @spacedock_add.route('/add', methods=['POST'])
 def add_hook():
-    raw = request.get_json(silent=True)
-
     # Submit add requests to queue in batches of <=10
-    messages = [batch_message(raw)]
+    messages = [batch_message(request.form)]
     for batch in sqs_batch_entries(messages):
         current_app.logger.info(f'Queueing add request batch: {batch}')
         current_app.config['client'].send_message_batch(
