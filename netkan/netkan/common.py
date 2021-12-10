129cd3da27a7d7d57d6e1683c8619edc4df7ccba
Paul Hebble <pjhebble@gmail.com>
2021-12-10 14:52:36 +0000

    Stream downloads

--- netkan/netkan/common.py
+++ netkan/netkan/common.py
@@ -1,11 +1,14 @@
-from typing import List, Iterable, Dict, Any
+from typing import List, Iterable, Dict, Any, IO
 import boto3  # pylint: disable=unused-import
 
+import requests
 import github
 from git import Repo
 
 from .metadata import Netkan
 from .repos import NetkanRepo
+
+USER_AGENT = 'Mozilla/5.0 (compatible; Netkanbot/1.0; CKAN; +https://github.com/KSP-CKAN/NetKAN-Infra)'
 
 
 def netkans(path: str, ids: Iterable[str]) -> Iterable[Netkan]:
@@ -39,3 +42,12 @@ def deletion_msg(msg: 'boto3.resources.factory.sqs.Message') -> Dict[str, Any]:
         'Id':            msg.message_id,
         'ReceiptHandle': msg.receipt_handle,
     }
+
+
+def download_stream_to_file(download_url: str, dest_file: IO[bytes]) -> None:
+    # Get big files in little chunks
+    with requests.get(download_url,
+                      headers={'User-Agent': USER_AGENT},
+                      stream=True) as req:
+        for chunk in req.iter_content(chunk_size=8192):
+            dest_file.write(chunk)
