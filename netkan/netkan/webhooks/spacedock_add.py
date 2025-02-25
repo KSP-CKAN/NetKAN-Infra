4e2ac3db0fc70a7f095b8452ba89a968c9cb08bc
Paul Hebble <pjhebble@gmail.com>
2025-02-25 09:08:04 -0600

    Log input and output of SpaceDock webhook for debugging

--- netkan/netkan/webhooks/spacedock_add.py
+++ netkan/netkan/webhooks/spacedock_add.py
@@ -1,6 +1,7 @@
 from hashlib import md5
 import json
 from typing import Tuple, TYPE_CHECKING
+import logging
 from flask import Blueprint, current_app, request
 from werkzeug.datastructures import ImmutableMultiDict
 
@@ -45,6 +46,10 @@ def add_hook(game_id: str) -> Tuple[str, int]:
 
 
 def batch_message(raw: 'ImmutableMultiDict[str, str]', game_id: str) -> SendMessageBatchRequestEntryTypeDef:
+    for key in ('username', 'user_github', 'user_forum_id',
+                'user_forum_username', 'email', 'user_url'):
+        logging.error('SpaceDockAdder form list %s: %s',
+                      key, json.dumps(raw.getlist(key)))
     body = json.dumps({**raw,
                        # Turn the separate user property lists into a list of user dicts so JSON can encode it
                        # (the original properties will only have the first user)
@@ -61,6 +66,7 @@ def batch_message(raw: 'ImmutableMultiDict[str, str]', game_id: str) -> SendMess
                                               raw.getlist('user_forum_username'),
                                               raw.getlist('email'),
                                               raw.getlist('user_url'))]})
+    logging.error('SpaceDockAdder queue message body: %s', body)
     return {
         'Id':                     '1',
         'MessageBody':            body,
